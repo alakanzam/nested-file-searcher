d6c700897edc089888c3cf68d00c5daae9e71ce0
linh.nguyen <lightalakanzam@gmail.com>
2019-09-27 19:51:29 +0700

    - Initial commit.

---
+++ ConsolePlayground/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace ConsolePlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var applicationPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var root = Path.Combine(applicationPath, "parent-folder");
+
+            var items = new List<string>();
+            var queue = new Queue<string>();
+            queue.Enqueue(root);
+
+            while (queue.TryDequeue(out var folder))
+            {
+                if (string.IsNullOrEmpty(folder))
+                    continue;
+
+                var directories = Directory.GetDirectories(folder);
+                var files = Directory.GetFiles(folder);
+                if (files != null && files.Length > 0)
+                    items.AddRange(files);
+
+                if (directories != null && directories.Length > 0)
+                {
+                    foreach (var directory in directories)
+                        queue.Enqueue(directory);
+
+                    items.AddRange(directories);
+                }
+            }
+
+            foreach (var item in items)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# Nested file searcher.
+
+## 1.Description:
+- A small application to search for all directories and files in a specific root folder without using recursive algorithm.
+
+
+## 2. How to run:
+- Open `PlaygroundConsole.sln` file.
+- Run `PlaygroundConsole` project.
+- A console application will be displayed with a list of folder and directories which have been defined by developer and copied to build folder.
